0e2afbd8ed1d8f05929dd7e6c7e726c3cfa8692d
likorsi <likorsi.sl@gmail.com>
2021-09-11 16:24:49 +0300

    добавлено сохранение данных после обновления

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict'
 
-const createTable = (captions, icons, data) => { 
+const createTable = (captions, icons, data, hidden, startIndex) => { 
 
-	const theadThs = captions.reduce( (prev, caption) => prev + `
+	const theadTh = thead || captions.reduce( (prev, caption) => prev + `
 		<th>${caption}
 		<div class="button-area">
 			<button class="svg-button hide" onclick="hideColumn(${captions.indexOf(caption)})">
@@ -18,31 +18,33 @@ const createTable = (captions, icons, data) => {
 	document.querySelector('.table').innerHTML = `
 		<table><thead>
 			<tr> 
-				${theadThs}
+				${theadTh}
 			</tr>
 		</thead>
 		<tbody>
 		</tbody>
 		</table>
 		<div class="button-area navigation">
-			<button class="svg-button" onclick="changePage(${"left"})">
+			<button class="svg-button" onclick="changePage('left')">
             	${icons.left}
 			</button>
-			<button class="svg-button sort" onclick="changePage(${"right"})">
+			<button class="svg-button sort" onclick="changePage('right')">
 				${icons.right}
 			</button>
-		</div>`
+		</div>
+				<button class="edit" onclick="makeOriginal()">replace with the original data</button>`
 
-	createTableBody(data)
+	// refresh('thead', thead)
+	createTableBody(refresh('data', data), refresh('hidden', hidden), refresh('startIndex', startIndex))
 }
 
-const createTableBody = (data, hide = false) => {
-	document.querySelector('tbody').innerHTML = data.reduce( (prev, elem) => prev +  `
+const createTableBody = (data, hidden, start) => {
+	document.querySelector('tbody').innerHTML = data.slice(startIndex, startIndex+10).reduce( (prev, elem) => prev +  `
   		<tr>
-      		<td>${elem.name.firstName}</td>
-      		<td>${elem.name.lastName}</td>
-      		<td><div class="clip">${elem.about}</div><span>...</span></td>
-      		<td>
+      		<td class="${hidden['firstName']}">${elem.name.firstName}</td>
+      		<td class="${hidden['lastName']}">${elem.name.lastName}</td>
+      		<td class="about ${hidden['about']}"><span class="clip ${hidden['about']}">${elem.about}</span><span class="ellipsis ${hidden['about']}">...</span></td>
+      		<td class="${hidden['eyeColor']}">
       			<div class="color" style="background-color: ${elem.eyeColor};">
       				<span>${elem.eyeColor}</span>
       			</div>
@@ -58,15 +60,14 @@ const sortColumn = (captionIndex) => {
 
 	data = bubbleSort(data, caption, isAlphaSort)
 
-	createTableBody(data)
-	// json = JSON.stringify(data)
-
+	createTableBody(refresh('data', data), refresh('hidden', hidden), refresh('startIndex', startIndex))
 	sortIcon.classList.toggle('sort-rotate') // поворот иконки сортировки
+	thead = document.querySelector('thead').innerHTML
+	refresh('thead', thead)
 }
 
 const bubbleSort = (data, caption, isAlphaSort) => {
 	const len = data.length - 1
-
 	for (let i = 0; i < len; i++) {
         for (let j = 0; j < len - i; j++) {
         	let current = data[j][caption] || data[j].name[caption] // страховка от undefind
@@ -83,8 +84,7 @@ const bubbleSort = (data, caption, isAlphaSort) => {
 	                data[j] = data[j + 1]
 	                data[j + 1] = swap
 	            }
-        	}
-            
+        	}     
         }
     }
     return data
@@ -92,24 +92,31 @@ const bubbleSort = (data, caption, isAlphaSort) => {
 
 const hideColumn = captionIndex => {
 	changeDataDiv.style.display = 'none'
+	const caption = captions[captionIndex].split(' ').join('')
 	const hideIcon = document.querySelector(`.table thead th:nth-child(${captionIndex+1}) .hide`)
 	hideIcon.innerHTML.trim() === icons.hideIcon ? hideIcon.innerHTML = icons.hideIconSplash : hideIcon.innerHTML = icons.hideIcon
 	hideIcon.classList.toggle('hide-splash')
 
-	const columnHead = document.querySelector(`.table thead th:nth-child(${captionIndex+1})`)
-	const columnBody = [...document.querySelectorAll(`.table tbody td:nth-child(${captionIndex+1})`)]
-	
-	columnBody.forEach( (elem, index) => {
-		elem.classList.toggle('visually-hidden')
-	});
+	hidden[caption] === 'visually-hidden' ? hidden[caption] = '' : hidden[caption] = 'visually-hidden'
 
-	columnHead.querySelectorAll('button')[1].classList.toggle('visually-hidden') // 
-	columnHead.classList.toggle('th-hide')
+	const columnToHide = document.querySelector(`.table thead th:nth-child(${captionIndex+1})`)
+	// const hiddenCaption = document.querySelector(`.table wrapper div:nth-child(${captionIndex+1})`)
 
-	// columnHead.querySelector('sort').classList.toggle('visually-hidden')
-	console.log(columnHead, columnHead.querySelectorAll('button')[1])
+	// const columns = [...document.querySelectorAll(`.table thead th`)]
+	// columnToHide.style.left = `${100 / 4}%`
+	// for(let i = captionIndex+1; i < columns.length; i++){
+	// 	columns[i].style.left = `-${100 / 4}%`
+	// }
+	// console.log(captionIndex)
 
+	columnToHide.querySelectorAll('button')[1].classList.toggle('visually-hidden') // 
+	columnToHide.classList.toggle('th-hide')
+	// hiddenCaption.classList.toggle('th-hide')
+	// hiddenCaption.classList.toggle('visually-hidden')
+	thead = document.querySelector('thead').innerHTML
+	refresh('thead', thead)
 
+	createTableBody(refresh('data', data), refresh('hidden', hidden), refresh('startIndex', startIndex))
 }
 
 const closeChangeDataDiv = () => changeDataDiv.style.display = 'none'
@@ -118,58 +125,65 @@ const handleInput = () => {
 	const form = document.querySelector('.change-data form')
 	const newValues = [...form.querySelectorAll('input'), form.querySelector('textarea')]
 	newValues.map( caption => {
-		// console.log(!!data[rowIndex][caption.id])
-		!!data[rowIndex][caption.id] 
-			? data[rowIndex][caption.id] = caption.value
-			: data[rowIndex].name[caption.id] = caption.value
+		!!data[rowIndex + startIndex][caption.id] 
+			? data[rowIndex + startIndex][caption.id] = caption.value
+			: data[rowIndex + startIndex].name[caption.id] = caption.value
 	})
 
-	// console.log(data[rowIndex])
-	createTableBody(data)
-	// json = JSON.stringify(data)
-	// json = data.stringify()
+	createTableBody(refresh('data', data), refresh('hidden', hidden), refresh('startIndex', startIndex))
+}
+
+const changePage = (side) => {
+	side === 'right'
+		? startIndex + 10 > data.length-1 ? startIndex = 0 : startIndex += 10
+		: startIndex - 10 < 0 ? startIndex = data.length-10 : startIndex -= 10
+	createTableBody(refresh('data', data), refresh('hidden', hidden), refresh('startIndex', startIndex))
+}
+
+const refresh = (key, item) => {
+	typeof item === 'object' ? localStorage.setItem(key, JSON.stringify(item)) : localStorage.setItem(key, item)
+
+	return typeof item === 'object' ? JSON.parse(localStorage.getItem(key)) : localStorage.getItem(key)
+}
+
+const makeOriginal = () => {
+	localStorage.removeItem('data')
+	localStorage.removeItem('thead')
+	localStorage.removeItem('startIndex')
+	localStorage.removeItem('hidden')
+
+	data = JSON.parse(json) 
+	startIndex = 0  
+	hidden = {'firstName': '','lastName': '','about': '','eyeColor': ''}
+	thead = ''
+
+	createTable(captions, icons, data, hidden, startIndex)
 }
 
 
-let data = JSON.parse(json)
-let rowIndex = -1 // переменная для запоминания строки, на которой произошел клик
-// console.log(JSON.stringify(data))
-// json = JSON.stringify(data)
+let data = JSON.parse(localStorage.getItem('data') || json) // 
+let startIndex = parseInt(localStorage.getItem('startIndex')) || 0 // позиция, с которой нужно показать постраничный вывод
+// 
+let hidden = JSON.parse(localStorage.getItem('hidden')) || {'firstName': '','lastName': '','about': '','eyeColor': ''}
+let thead = localStorage.getItem('thead') || ''
+let rowIndex = -1 // 
 
 const re = new RegExp('(?=[A-Z])') // regExp для разделения названий через заглавную
 const captions = Object.keys(data[0].name).concat(Object.keys(data[0]).slice(-2)).map( caption => {
 	return caption.split(re).join(' ')
 })
 
-createTable(captions, icons, data) // создаем и наполняем таблицу
-
-// addEllipsis() //
-
+createTable(captions, icons, data, hidden, startIndex) // создаем и наполняем таблицу
 const changeDataDiv = document.querySelector('.change-data') // получаем форму редактирования
-// console.log(changeDataDiv)
 
 // заполнение формы данными из строки
 document.querySelector('tbody').onclick = (evt) => {
 	rowIndex = (evt.target.parentNode.rowIndex || evt.target.parentNode.parentNode.rowIndex) - 1
-	// changeDataDiv.style.display = (changeDataDiv.style.display == 'none') ? '' : 'none'
 	const form = document.querySelector('.change-data form')
-	form.querySelector('#firstName').value = data[rowIndex].name.firstName
-	form.querySelector('#lastName').value = data[rowIndex].name.lastName
-	form.querySelector('#about').value = data[rowIndex].about
-	form.querySelector('#eyeColor').value = data[rowIndex].eyeColor
-
+	console.log(rowIndex + startIndex, startIndex, data[rowIndex + startIndex -1 ].name.firstName)
+	form.querySelector('#firstName').value = data[rowIndex + startIndex].name.firstName
+	form.querySelector('#lastName').value = data[rowIndex + startIndex].name.lastName
+	form.querySelector('#about').value = data[rowIndex + startIndex].about
+	form.querySelector('#eyeColor').value = data[rowIndex + startIndex].eyeColor
 	changeDataDiv.style.display = 'block'
-
-	// console.log(data[rowIndex])	
 }
-
-// window.onclick = (evt) => {
-// 	const isFreeArea = evt.target.localName === 'html' || evt.target.localName === 'body'
-// 	if (isFreeArea) {
-// 		changeDataDiv.style.display = 'none'
-// 	}
-// }
-
-// console.log(tbody)
-
-
